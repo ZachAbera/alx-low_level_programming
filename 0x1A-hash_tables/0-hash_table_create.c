ae305fd3d7cf100a03c689c1c4cc92498b308568
Zekarias Tesfaye Abera <118669208+ZeekJager@users.noreply.github.com>
2023-07-06 21:27:37 +0300

    Update new

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,27 +1,34 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_create - Creates a hash table.
- * @size: The size of the array.
- *
- * Return: If an error occurs - NULL.
- *         Otherwise - a pointer to the new hash table.
+ * hash_table_create - create a hash table of BUFF_SIZE
+ * @size: len of new hash tab
+ * Return: new hash tab
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
+	hash_table_t *new_tab = NULL;
 	unsigned long int i;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
+	/* create table hash */
+	new_tab = malloc(sizeof(hash_table_t));
+	if (new_tab == NULL)
 		return (NULL);
 
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
+	/* create array for each node in the table hash */
+	new_tab->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_tab->array == NULL)
+	{
+		free(new_tab);
 		return (NULL);
+	}
+
 	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
+	{
+		new_tab->array[i] = NULL;
+	}
+	/* save len of table_hash create */
+	new_tab->size = size;
 
-	return (ht);
+	return (new_tab);
 }
